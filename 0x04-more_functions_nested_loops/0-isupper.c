e5de0ffc5ecd7f0e02410290a3d840faa0399c04
Chidubemj <jude.anyaegbu@yahoo.com>
2023-10-13 13:07:08 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,28 +6,11 @@
  *
  * Return: 0 or 1
  */
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	return (1);
+		return (1);
 	else
-	return (0);
-}
-
-
-Task 1
-#include "main.h"
-/**
- * _isdigit - check if numbers are 0 - 9
- * @c: char to be checked
- *
- * Return: 0 or 1
- */
-
-int _isdigit(int c)
-{
-	if (c >= '0' && c <= '9')
-	return (1);
-	else
-	return (0);
+		return (0);
 }
